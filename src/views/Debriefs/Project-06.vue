40cf9e26b1551cdb97db33058be5930140a7fb78
Matthew <matthew.sanford@rebilly.com>
2024-10-17 11:50:46 -0400

    project 6 debrief

--- src/views/Debriefs/Project-06.vue
+++ src/views/Debriefs/Project-06.vue
@@ -4,6 +4,12 @@ import Debrief from '../../components/Debrief.vue';
 
 <template>
   <Debrief title="Project 6: Deposits" :team="['Arman Tuyakbayev', 'Matthew Sanford']">
-    <p>Missing data</p>
+    <template #made-you-smile>
+      <p>We created a subscrioption, than use rebilly instruments money flow and the instruments event
+        `purchase-completed` to then use API to call transactionApply to the subscriptions invoice.</p>
+    </template>
+    <template #find-confusing>
+      <p>finding this solution required our experience at Rbbilly to solve and would not come to a regular merchant</p>
+    </template>
   </Debrief>
 </template>
