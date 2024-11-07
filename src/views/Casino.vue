50ee3daca8329fb4724e901d8713737ea3b2c45b
Matthew Sanford <matthew.sanford@rebilly.com>
2024-11-07 16:43:12 +0000

    p14

--- src/views/Casino.vue
+++ src/views/Casino.vue
@@ -1,29 +1,38 @@
 <script setup>
-import { onMounted } from "vue";
+import { onMounted, reactive } from "vue";
 import RebillyInstrumentsLayout from "../components/RebillyInstrumentsLayout.vue";
 import { useBaseConfig, websiteId } from "../components/RebillyInstruments.js";
 import { useRebillySDK } from "../vendor/rebilly-js-sdk/index.js";
 
 const api = useRebillySDK();
+const state = reactive({
+  currency: 'USD',
+  token: null
+})
 
 onMounted(async () => {
-  const { token } = await api.actions.passwordlessLogin({
+  const password = await api.actions.passwordlessLogin({
     customerId: "cus_01J56B1FAQ1M4Y29E3VWCB41EQ",
   });
+  state.token = password.token;
 
-  const { fields: customerFields } = await api.customers.get({
-    id: "cus_01J56B1FAQ1M4Y29E3VWCB41EQ",
-  });
-  console.log(customerFields);
+  changeCurrency(state.currency);
+});
+
+const changeCurrency = async (currency) => {
+  state.currency = currency;
+  if (RebillyInstruments.state.hasMounted) {
+    RebillyInstruments.destroy();
+  }
   const strategyId =
-    customerFields.customFields.defaultCurrency === "USD"
-      ? "dep_str_01JATHGCV7GZ9XJXH1MQ5CRXYN"
-      : "dep_str_01JATHJ48E2YS93BVZPH30S87N";
+    state.currency === "CAD"
+      ? "dep_str_01JATHJ48E2YS93BVZPH30S87N"
+      : "dep_str_01JATHGCV7GZ9XJXH1MQ5CRXYN";
 
   const requestDepositData = {
     websiteId: websiteId,
     customerId: "cus_01J56B1FAQ1M4Y29E3VWCB41EQ",
-    currency: customerFields.customFields.defaultCurrency,
+    currency: state.currency,
     strategyId: strategyId,
   };
   const { fields: depositFields } = await api.depositRequests.create({
@@ -34,16 +43,34 @@ onMounted(async () => {
     ...useBaseConfig({
       deleteKeys: ["publishableKey", "websiteId", "organizationId"],
     }),
-    jwt: token,
+    jwt: state.token,
     deposit: {
       depositRequestId: depositFields.id,
     },
   };
 
   RebillyInstruments.mount(config);
-});
+}
 </script>
 
 <template>
-  <RebillyInstrumentsLayout></RebillyInstrumentsLayout>
+  <RebillyInstrumentsLayout>
+    <template #pre-form>
+      <button
+        class="rebilly-instruments-button"
+        :class="{ 'rebilly-instruments-button-secondary': state.currency !== 'USD' }"
+        @click="() => changeCurrency('USD')"
+      >USD</button>
+      <button
+        class="rebilly-instruments-button"
+        :class="{ 'rebilly-instruments-button-secondary': state.currency !== 'CAD' }"
+        @click="() => changeCurrency('CAD')"
+      >CAD</button>
+      <button
+        class="rebilly-instruments-button"
+        :class="{ 'rebilly-instruments-button-secondary': state.currency !== 'EUR' }"
+        @click="() => changeCurrency('EUR')"
+      >EUR</button>
+    </template>
+  </RebillyInstrumentsLayout>
 </template>
---
+++ src/views/Debriefs/Project-14.vue
@@ -0,0 +1,28 @@
+<script setup>
+import Debrief from "../../components/Debrief.vue";
+</script>
+
+<template>
+  <Debrief
+    title="Project 13: Casino deposit currency"
+    :team="['Samuel Lafrenaye', 'Matthew Sanford']"
+  >
+    <template #made-you-smile>
+      <p>??</p>
+    </template>
+    <template #find-confusing>
+      <ul>
+        <li>Did not natually find the docs. Deposits did not mention DCC at all.</li>
+        <li>We could unslect force conversions, we had to remove DCC and re-add it</li>
+        <li>Can't cancel once DCC is offered</li>
+        <li>DCC shows different locale than instruments</li>
+        <li>We were confused abot how the 2% markup would show up</li>
+        <li>We assumed that 2% would show up on base amount instead of the conversion rate</li>
+        <li>We did not know how to have the customer local currency show up</li>
+        <li>We were confused about the local currency and seemed like it was wrong in the DCC offer</li>
+        <li>Docs not clear about what is expected or logic is wrong</li>
+        <li>We have to specifiy the expected currency but we can't do a conversion on that currecy if it's accepted</li>
+      </ul>
+    </template>
+  </Debrief>
+</template>
