8ed5f7a87623b914794fe5da504cc3bd93e9754d
Matthew Sanford <matthew.sanford@rebilly.com>
2024-12-13 16:11:19 +0000

    p19

---
+++ src/views/Debriefs/Project-19.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Debrief from "../../components/Debrief.vue";
+</script>
+
+<template>
+    <Debrief title="Project 19: VIP customers" :team="['Samuel Lafrenaye', 'Cesar Level', 'Matthew Sanford']">
+        <template #made-you-smile>
+            <p></p>
+        </template>
+        <template #find-confusing>
+            <ul>
+                <li>Searching the doc for `Lifetime Revenue` gave article about KPI</li>
+                <li>We receive a 404 for GET deposit request after creating a one on any customer except a specific customer</li>
+                <li>Consfusion between Lifetime Revenue > Amount USD and just Amount. Only "Amount" worked and "Amount USD" did not even with USD currency</li>
+                <li>Tested with one customer by toggleing on off the VIP tag but other customers do not work</li>
+            </ul>
+        </template>
+    </Debrief>
+</template>
