a626cb18fa9d48330ed82243ed3ac6cb26454c3a
Matthew Sanford <matthew.sanford@rebilly.com>
2025-02-14 20:45:47 +0000

    p26 - google sheets integration

--- src/components/DebriefItem.vue
+++ src/components/DebriefItem.vue
@@ -5,117 +5,117 @@ import MarkdownIt from "markdown-it";
 const slots = useSlots();
 
 const props = defineProps({
-    suggestion: {
-        type: Boolean,
-    },
-    bug: {
-        type: Boolean,
-    },
-    improvement: {
-        type: Boolean,
-    },
-    comment: {
-        type: Boolean,
-    },
-    question: {
-        type: Boolean,
-    }
+  suggestion: {
+    type: Boolean,
+  },
+  bug: {
+    type: Boolean,
+  },
+  improvement: {
+    type: Boolean,
+  },
+  comment: {
+    type: Boolean,
+  },
+  question: {
+    type: Boolean,
+  },
 });
 const legend = computed(() => {
-    if (props.comment) {
-        return 'Comment';
-    }
-    if (props.suggestion) {
-        return 'Suggestion';
-    }
-    if(props.bug) {
-        return 'Bug';
-    }
-    if (props.improvement) {
-        return 'Improvement';
-    }
-    if (props.question) {
-        return 'Question ?';
-    }
-    return 'Comment'
+  if (props.comment) {
+    return "Comment";
+  }
+  if (props.suggestion) {
+    return "Suggestion";
+  }
+  if (props.bug) {
+    return "Bug";
+  }
+  if (props.improvement) {
+    return "Improvement";
+  }
+  if (props.question) {
+    return "Question ?";
+  }
+  return "Comment";
 });
 const slotData = computed(() => {
-    let markdown = slots.default()[0].children;
-    markdown = markdown.replace(/^[ \t]+/gm, '');
-    if (markdown && markdown.length > 0) {
-        return MarkdownIt().render(markdown);
-    }
-})
+  let markdown = slots.default()[0].children;
+  markdown = markdown.replace(/^[ \t]+/gm, "");
+  if (markdown && markdown.length > 0) {
+    return MarkdownIt().render(markdown);
+  }
+});
 
 const computedClass = computed(() => {
-    if (props.comment) {
-        return 'comment';
-    }
-    if (props.suggestion) {
-        return 'suggestion';
-    }
-    if(props.bug) {
-        return 'bug';
-    }
-    if(props.improvement) {
-        return 'improvement';
-    }
-    if(props.question) {
-        return 'question';
-    }
-    return 'comment'
+  if (props.comment) {
+    return "comment";
+  }
+  if (props.suggestion) {
+    return "suggestion";
+  }
+  if (props.bug) {
+    return "bug";
+  }
+  if (props.improvement) {
+    return "improvement";
+  }
+  if (props.question) {
+    return "question";
+  }
+  return "comment";
 });
 </script>
 
 <template>
-    <fieldset :class="computedClass">
-        <legend>{{ legend }}</legend>
-        <div class="markdown" v-html="slotData"></div>
-    </fieldset>
-    <code style="display: none;"><slot></slot></code>
+  <fieldset :class="computedClass">
+    <legend>{{ legend }}</legend>
+    <div class="markdown" v-html="slotData"></div>
+    <code style="display: none"><slot></slot></code>
+  </fieldset>
 </template>
 
 <style scoped>
 fieldset {
-    border-radius: 5px;
-    border-width: 3px;
-    border-style: solid;
-    
-    margin-top: 1rem;
+  border-radius: 5px;
+  border-width: 3px;
+  border-style: solid;
 
+  margin-top: 1rem;
+
+  legend {
+    font-weight: bold;
+  }
+
+  &.comment {
+    border-color: cornflowerblue;
     legend {
-        font-weight: bold;
+      color: cornflowerblue;
     }
-
-    &.comment {
-        border-color: cornflowerblue;
-        legend {
-            color: cornflowerblue;
-        }
+  }
+  &.bug {
+    border-color: tomato;
+    legend {
+      color: tomato;
     }
-    &.bug {
-        border-color: tomato;
-        legend {
-            color: tomato;
-        }
+  }
+  &.suggestion {
+    border-color: slateblue;
+    legend {
+      color: slateblue;
     }
-    &.suggestion {
-        border-color: slateblue;
-        legend {
-            color: slateblue;
-        }
+  }
+  &.improvement {
+    border-color: mediumseagreen;
+    legend {
+      color: mediumseagreen;
     }
-    &.improvement {
-        border-color: mediumseagreen;
-        legend {
-            color: mediumseagreen;
-        }
+  }
+  &.question {
+    border-color: orange;
+    legend {
+      color: orange;
     }
-    &.question {
-        border-color: orange;
-        legend {
-            color: orange;
-        }
-    }
+  }
 }
 </style>
---
+++ src/views/Debriefs/Project-26.vue
@@ -0,0 +1,53 @@
+<script setup>
+import Debrief from "../../components/Debrief.vue";
+import DebriefItem from "../../components/DebriefItem.vue";
+</script>
+
+<template>
+  <Debrief
+    title="Project 26: Google Sheets integration"
+    :team="['Mike Nguyen', 'Matthew Sanford']"
+  >
+    <template #made-you-smile>
+      <p>
+        The docs were easy to find and made integration very straight forward.
+      </p>
+    </template>
+    <template #find-confusing>
+      <debrief-item bug>
+        Google app has a warning that the app is not verified
+
+        ![](/p26/verified.png)
+      </debrief-item>
+
+      <debrief-item improvement>
+        Google app has overly permissive permissions. These should be reduced in
+        scope if able
+        
+        ![](/p26/permissions.png)
+      </debrief-item>
+
+      <debrief-item improvement>
+        When adding a field the option will reorder as you interact. should only
+        reorder when it initaly is opened
+        
+        ![](/p26/jumping_option.gif)
+      </debrief-item>
+
+      <debrief-item suggestion>
+        Test integration should provide better default dummy data.
+        
+        First time the modal showed up we tested without any modification and an empty row
+        was added to the sheet
+      </debrief-item>
+
+      <debrief-item improvement>
+        After initalzing a test run we got ~107 rows added to the sheet but we
+        expected only 1 row. 
+        
+        Also after some time we seemed to receive more rows
+        but we were not able to reproduce that.
+      </debrief-item>
+    </template>
+  </Debrief>
+</template>
