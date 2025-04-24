43aff3975911dd2d645681dc596a986e32668c80
Matthew Sanford <matthew.sanford@rebilly.com>
2025-04-24 19:09:33 +0000

    p36

---
+++ src/views/Debriefs/Project-36.vue
@@ -0,0 +1,24 @@
+<script setup>
+import Debrief from "../../components/Debrief.vue";
+import DebriefItem from "../../components/DebriefItem.vue";
+</script>
+
+<template>
+  <Debrief
+    title="Project 36: Custom domains"
+    :team="['Francis Dortort', 'Matthew Sanford']"
+  >
+    <template #made-you-smile>
+      <p>SSL Certificates auto generation works well</p>
+    </template>
+    <template #find-confusing>
+       <debrief-item bug>
+         Recomm / transaction.approvalURL does not include the custom domain but will redirect correctly.
+
+         ![](/p36/recomm-modal.png)
+         ![](/p36/transaction-approvalUrl.png)
+         ![](/p36/correct-url.png)
+       </debrief-item>
+    </template>
+  </Debrief>
+</template>
