35b2687ef314d23fa0037c80b3548aeb62414ab2
andrew.lepekha <andrew.lepekha@rebilly.com>
2025-05-22 10:18:43 -0700

    Add Checkout.vue

---
+++ src/views/Checkout.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { reactive, onMounted } from 'vue';
+import RebillyInstrumentsLayout from '../components/RebillyInstrumentsLayout.vue';
+import { useBaseConfig } from '@/components/RebillyInstruments';
+
+
+onMounted(() => {
+  const config = {
+    ...useBaseConfig(),
+    items: [
+      {
+        planId: "beginner",
+        quantity: 1,
+      },
+    ],
+  };
+
+  RebillyInstruments.mount(config);
+});
+
+</script>
+
+<template>
+  <main>
+    <RebillyInstrumentsLayout>
+    </RebillyInstrumentsLayout>
+  </main>
+</template>
