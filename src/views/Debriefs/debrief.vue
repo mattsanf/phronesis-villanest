5bb945ca9de910a80962a3190f28785224190e74
Matthew Sanford <matthew.sanford@rebilly.com>
2025-07-15 14:37:20 -0400

    use import meta glob

--- src/views/Debriefs/debrief.vue
+++ src/views/Debriefs/debrief.vue
@@ -4,13 +4,18 @@ import { useRoute } from 'vue-router';
 import Markdown from "@/components/Markdown.vue";
 
 const markdown = ref("");
-
 const {params: {slug}} = useRoute();
+const markdownFiles = import.meta.glob('./*.md', { as: 'raw' });
 
 onMounted(async () => {
     document.title = `Debrief: ${slug}`;
-    const response = await fetch(`/src/views/Debriefs/${slug}.md`);
-    markdown.value = await response.text();
+    const filePath = `./${slug}.md`;
+    
+    if (markdownFiles[filePath]) {
+        markdown.value = await markdownFiles[filePath]();
+    } else {
+        markdown.value = null;
+    }
 })
 </script>
 
